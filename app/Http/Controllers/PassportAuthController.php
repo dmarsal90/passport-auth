38b5dc3ed86dadfbe7c308de91eb2a343d58a5be
David Marsal Ivars <davidmarsalivars@gmail.com>
2022-12-29 01:33:44 -0500

    Add register function to PassportAuthController

--- app/Http/Controllers/PassportAuthController.php
+++ app/Http/Controllers/PassportAuthController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules\Password;
 
 class PassportAuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        //generate token
+        $token = $user->createToken('Token')->accessToken;
+
+        return response()->json(['token' => $token], 200);
+    }
 }
